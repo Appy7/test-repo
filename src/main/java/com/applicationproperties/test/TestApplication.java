40cb6530ba374b322a3646615bb0244ccc38e76b
Appy7 <aparna.samal32@gmail.com>
2022-11-13 17:27:58 +0530

    first commit

---
+++ src/main/java/com/applicationproperties/test/TestApplication.java
@@ -0,0 +1,31 @@
+package com.applicationproperties.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TestApplication.class, args);
+
+		//List<Integer> i= Arrays.asList(9,8,1,2,3,4,5);
+		//Integer k = i.stream().min((a,b)->a.compareTo(b)).get();
+		//System.out.println(k);
+
+		System.out.println("Enter the String");
+		Scanner scanner = new Scanner(System.in);
+
+			System.out.println(" String is" + scanner);
+
+
+
+	}
+
+}
---
+++ src/main/java/com/applicationproperties/test/test.java
@@ -0,0 +1,19 @@
+package com.applicationproperties.test;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.DenyAll;
+
+@RestController
+
+
+public class test {
+
+
+
+
+}
